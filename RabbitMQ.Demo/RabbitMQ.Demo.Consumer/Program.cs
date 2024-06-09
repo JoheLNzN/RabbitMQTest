678cd9142bb75294e697a23fe1b44f43f7f4b02a
Joel C. Naupa Crispín <jnaupac@gmail.com>
2024-06-09 11:29:16 -0500

    Add Publisher and Consumer

---
+++ RabbitMQ.Demo/RabbitMQ.Demo.Consumer/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new()
+{
+    HostName = "localhost",
+    UserName = "admin",
+    Password = "admin"
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "queuedemo",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += Consumer_Received;
+
+channel.BasicConsume(
+    queue: "queuedemo",
+    autoAck: true,
+    consumer
+);
+
+void Consumer_Received(object? sender, BasicDeliverEventArgs e)
+{
+    var body = e.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+
+    Console.WriteLine($"[x] Received: {message}");
+}
+
+Console.ReadLine();
---
+++ RabbitMQ.Demo/RabbitMQ.Demo.Publisher/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new() { 
+    HostName = "localhost", 
+    UserName = "admin", 
+    Password = "admin" 
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "queuedemo",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+);
+
+string message = "Funciona mi comunicación entre Pubisher => Consumer";
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(
+    exchange: "", 
+    routingKey: "queuedemo", 
+    basicProperties: null, 
+    body
+);
+
+Console.WriteLine($"[x] Send: {message}");
+Console.ReadLine();
